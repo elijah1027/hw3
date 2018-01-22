16106a33809f00cf3bdd291760ae36af8e7c2257
elijah1027 <31459838+elijah1027@users.noreply.github.com>
2018-01-22 15:25:24 -0500

    Add files via upload

--- hw3_friend.py
+++ hw3_friend.py
@@ -2,45 +2,22 @@
 # coding: utf-8
 
 # In[ ]:
-
-
 import re
 import sys
 from pyspark import SparkConf, SparkContext
+from itertools import combinations
 conf = SparkConf()
 sc = SparkContext(conf=conf)
 lines = sc.textFile(sys.argv[1])
 user_friend = lines.map(lambda l:tuple(l.split('\t')))
 user_ratingprod = user_friend.flatMap(lambda x:[((x[0],i),-1000) for i in x[1].split(',')])
-user_ratingprod2 = user_friend.flatMap(lambda x: (lambda j:[[((j[u],j[i]),1) for i in range(u+1,len(j))]for u in range(0,len(j))])(x[1].split(',')))
-user_ratingprod2 = user_ratingprod2.flatMap(lambda xs: [x for x in xs])
-unio = user_ratingprod.union(user_ratingprod2).reduceByKey(lambda n1,n2:n1+n2)
-unio = unio.map(lambda x: (x[0][0],x[0],x[1]))
-key = unio.map(lambda x: (x[0]))
-lst = key.collect()
-uni_list = []
-for i in lst:
-    if i not in uni_list:
-        uni_list.append(i)
-charRDD = []
-
-for i in uni_list:
-    char = i
-    user = unio.filter(lambda x: x[0]==char)
-    top_10 = user.filter(lambda u:u[2] >= 0).takeOrdered(10, key=lambda x: -x[2])
-    if len(top_10) == 10:
-        charRDD.append(top_10)
-    else:
-        user_friend = user.map(lambda x:x[1][1]).collect()
-        
-        for j in uni_list:
-            if j not in user_friend:
-                top_10.append((char,(char,j),"random friend"))
-                if len(top_10) == 10:
-                    charRDD.append(top_10)
-                    break
-
-charRDD = sc.parallelize(charRDD)
-charRDD.saveAsTextFile(sys.argv[2])
+u_friend = user_friend.map(lambda x:(x[1].split(',')))
+user_ratingprod2 = u_friend.flatMap(lambda w:[(i,1) for i in combinations(w,2)])
+unio = user_ratingprod.union(user_ratingprod2).reduceByKey(lambda n1,n2:n1+n2).map(lambda x: (x[0][0],(x[0][0],x[0][1],x[1])))
+result = unio.filter(lambda u:u[1][2] >= 0).groupByKey().map(lambda v: sorted(v[1], key=lambda k: k[2], reverse=True)).map(lambda x: x[0:10])
+result.saveAsTextFile(sys.argv[2])
 sc.stop()
+
+
+#~/opt/spark/bin/spark-submit count_words.py hw3_friend.txt ~/spark_code/final/
 
